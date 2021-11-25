22fdea9100075cf538522ee8890c9c4984a38619
Ron Schmitt <ron.schmit@caa.columbia.edu>
2021-11-24 23:06:00 -0500

    minor mods

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quickstart'`, () => {
+  it(`should have as title 'demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('quickstart');
+    expect(app.title).toEqual('demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('quickstart app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo app is running!');
   });
 });
