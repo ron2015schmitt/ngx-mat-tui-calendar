44578b8bd40e6550b94c75b3a8e07bfb91ec1152
Ron Schmitt <ron.schmit@caa.columbia.edu>
2021-12-09 11:31:28 -0500

    added components to public api

--- projects/ngx-mat-tui-calendar/src/public-api.ts
+++ projects/ngx-mat-tui-calendar/src/public-api.ts
@@ -2,9 +2,12 @@
  * Public API Surface of ngx-mat-tui-calendar
  */
 
+export * from './lib/ngx-mat-tui-calendar.module';
 export * from './lib/ngx-mat-tui-calendar.service';
 export * from './lib/ngx-mat-tui-calendar.component';
-export * from './lib/ngx-mat-tui-calendar.module';
+export * from './lib/ngx-mat-tui-calendar-editor-dialog/ngx-mat-tui-calendar-editor-dialog.component';
+export * from './lib/ngx-mat-tui-calendar-wrapper/ngx-mat-tui-calendar-wrapper.component';
+
 export * from './lib/calendar-options';
 export * from './lib/calendar-editor-options';
 export * from './lib/local-date';
